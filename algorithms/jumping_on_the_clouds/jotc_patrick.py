085fc5280014a94f9f946d0ac93ee3513df53f67
Patrick McNally <Patrick.J.McNally@gmail.com>
2017-01-15 03:42:13 +0000

    move all problems to folders

---
+++ algorithms/jumping_on_the_clouds/jotc_patrick.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+def main():
+    energy = 100
+    num_clouds, jump_dist = (int(x) for x in input().strip().split(' '))
+    clouds = [int(x) for x in input().strip().split(' ')]
+    
+    index = 0
+    
+    while True:
+        index = (index + jump_dist) % num_clouds
+        energy -= (1 + clouds[index]*2)
+        
+        if index == 0:
+            return energy
+        
+print(main())
