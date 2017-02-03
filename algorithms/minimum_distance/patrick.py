24120ae240460b2c6658bb06f5de7e58ba075db8
Patrick McNally <Patrick.J.McNally@gmail.com>
2017-02-03 00:09:39 -0600

    Update solution

--- algorithms/minimum_distance/patrick.py
+++ algorithms/minimum_distance/patrick.py
@@ -1,28 +1,39 @@
 #!/bin/python3
 
 def main():
+    # Function to gather inputs and print output
     junk = int(input().strip())
     array = [int(x) for x in input().strip().split(' ')]
     print(min_dist(array))
     
 def min_dist(array):
-    min_dist = None
+    # Min dist must be less than length of array.  Initialize here as impossible greater value.
+    # Add 1 to protect check below "If min_dist == 1" from len 1 array
+    array_len = len(array)
+    min_dist = array_len
+    
+    # dictionary to hold value and last seen index
     index_dict = {}
+    
+    # Itterate over every index, elem in list.  
     for index, num in enumerate(array):
         temp_index = index_dict.get(num)
+        # Check if value already seen
         if temp_index == None:
+            # If not save to dict.
             index_dict[num] = index
-        else:
+        else: # if already seen compare new dist to min dist and replace or not.
             temp_dist = index - temp_index
-            try:
-                if temp_dist < min_dist:
-                    min_dist = temp_dist
-            except:
+            if temp_dist < min_dist:
                 min_dist = temp_dist
+        
+        # 1 is the smallest possible dist.  No need to keep looking
+        if min_dist == 1:
+            break
     
-    if min_dist:
-        return min_dist
-    else:
+    if min_dist == array_len:
         return -1
+    else:
+        return min_dist
     
 main()
