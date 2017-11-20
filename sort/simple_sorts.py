7bf54b53008b7d6d7cde60f67ac58d745d36ba5c
Patrick McNally <Patrick@pjmcnally.net>
2017-11-20 01:18:48 -0600

    add cocktail_selection sort

--- sort/simple_sorts.py
+++ sort/simple_sorts.py
@@ -17,3 +17,30 @@ def selection_sort(arr):
                 min_i = j
 
         arr[i], arr[min_i] = arr[min_i], arr[i]
+
+def cocktail_selection(arr):
+    """
+    Cocktail selection sort works like cocktail bubble sort.
+
+    In a normal selection sort each pass over the list you find the min (or max)
+    value and place it at the beginning (or end) of list.  Each pass sorts 1
+    item.
+
+    Cocktail selection sorts find both the min and max values and places both
+    at the beginning and end respectively.  This should reduce the total loops
+    required over the list by 1/2 as 2 items are sorted per pass.
+    """
+    n = len(arr)
+    for i in range(n//2):
+        min_i = i
+        max_i = i
+        for j in range(i + 1, n - i):
+            if arr[j] < arr[min_i]:
+                min_i = j
+            elif arr[j] > arr[max_i]:
+                max_i = j
+
+        arr[i], arr[min_i] = arr[min_i], arr[i]
+        if i == max_i:
+            max_i = min_i
+        arr[n - 1 - i], arr[max_i] = arr[max_i], arr[n - 1 - i]
