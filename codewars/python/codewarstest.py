30527a72e572226176258d20db5a9b4da3e176cf
Patrick McNally <Patrick.J.McNally@gmail.com>
2018-09-28 23:40:02 -0500

    Update file name

--- codewars/python/codewarstest.py
+++ codewars/python/codewarstest.py
@@ -1,8 +1,10 @@
+"""Adapt unittest to CodeWars style tests."""
 import unittest
-
-test = unittest.TestCase()
 
 
 def assert_equals(val, result, message_fail=''):
+    """Adapate CodeWars assert_equals func to unittests."""
+    test = unittest.TestCase()
+
     test.assertEqual(val, result, message_fail)
     print(f"{val} = {result}: Test Passed")
