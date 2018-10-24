3de874e8ba7ef8c911cc373fea65e216174991ef
Patrick McNally <pmcnally@blackhillsip.com>
2018-10-24 18:47:00 -0500

    Reorganize functions

--- misc/lotto/lotto.py
+++ misc/lotto/lotto.py
@@ -9,29 +9,17 @@ class Lotto():
         """Init function."""
         # Setup pool of possible numbers
         self.num_max = 70
-        self.extra_num_max = 25
-        self.nums = range(1, self.num_max + 1)
-
-        # Setup rules to select nums
         self.num_selected = 5
-        self.winning_nums = []
+        self.nums = range(1, self.num_max + 1)
+        self.extra_num_max = 25
 
-    def run_lottery(self):
-        """Run simulated lottery."""
-        # Generate and display winning numbers
+        # Get winning numbers
+        self.winners = 0
+        self.winners_wanted = 1
         self.winning_nums = self.generate_ticket()
-        print("\r\nWinning ticket: ", end="")
-        print(" ".join(f"{x:02}" for x in self.winning_nums))
 
-        # Attempt to match winning numbers. Stop when winning ticket found.
-        counter = 0
-        while True:
-            counter += 1
-            print("\rTickets tried: {0:012,}".format(counter), end="")
-            if self.generate_ticket() == self.winning_nums:
-                print("\r\n\r\nWinner Found!")
-                print(f"Total cost to win: ${counter*2:.2f}")
-                break
+        # Set
+        self.tickets_generated = 0
 
     def generate_ticket(self):
         """Generate lottery ticket."""
@@ -39,6 +27,35 @@ class Lotto():
         ticket.append(randint(1, self.extra_num_max))
 
         return ticket
+
+    def display_start(self):
+        """Display winning numbers."""
+        print("\r\nWinning ticket: ", end="")
+        print(" ".join(f"{x:02}" for x in self.winning_nums))
+
+    def find_winners(self):
+        """Generate tickets to find winners. Stop when desired num found."""
+        while self.winners < self.winners_wanted:
+            self.tickets_generated += 1
+            if self.generate_ticket() == self.winning_nums:
+                self.winners += 1
+
+            print(
+                "\rTickets tried: {0:012,}. Winners found: {1:,}".format(
+                    self.tickets_generated, self.winners),
+                end="")
+
+    def display_end(self):
+        """Display final results."""
+        print("\r\n\r\nLottery Over!")
+        print("Average tickets per winner: {0:,}".format(
+            self.tickets_generated // self.winners))
+
+    def run_lottery(self):
+        """Run simulated lottery."""
+        self.display_start()
+        self.find_winners()
+        self.display_end()
 
 
 def main():
