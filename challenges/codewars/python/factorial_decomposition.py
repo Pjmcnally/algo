f8b1fd73d693428d6e0c26f86c2b5ce06163d926
Patrick McNally <Patrick@pjmcnally.net>
2019-06-20 12:13:13 -0500

    Create factorial_decomposition.py

---
+++ challenges/codewars/python/factorial_decomposition.py
@@ -0,0 +1,36 @@
+def decomp(n):
+    # Calculate all needed primes once to save time
+    # We only need the primes up to the square root of the original number.
+    primes = primes_less_than(int(n ** 0.5))
+
+    # Calculate all prime factors using precalculated primes
+    factors = {}
+    for num in range(2, n + 1):
+        for prime in primes:
+            while num % prime == 0:
+                num //= prime
+                factors[prime] = factors.get(prime, 0) + 1
+
+        # Add remaining value if that value is not 1 (as it must be prime)
+        if num != 1:
+            factors[num] = factors.get(num, 0) + 1
+
+    # Format and return output
+    facts = sorted(factors.items())
+    return " * ".join([f"{num}^{cnt}" if cnt > 1 else f"{num}" for num, cnt in facts])
+
+
+def primes_less_than(x):
+    """Sieve of Eratosthenes to calculate all prime numbers less than or equal to x."""
+    # Create list to represent primality of numbers (Set 0 and 1 to False)
+    primes = [True] * (x + 1)
+    primes[0], primes[1] = False, False
+
+    # For each prime number cross out all multiples of that number
+    for i in range(int(x ** 0.5) + 1):
+        if primes[i]:
+            for num in range(i * 2, len(primes), i):
+                primes[num] = False
+
+    # Convert indexes to numbers and return as list
+    return [i for i, x in enumerate(primes) if x]
