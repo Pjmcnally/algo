5e5c32097fdb86991155482eccdf3c5e5fa712be
Pjmcnally <Patrick.J.McNally@gmail.com>
2023-07-31 03:54:04 -0500

    Add comments

--- misc/threes/test.py
+++ misc/threes/test.py
@@ -3,6 +3,9 @@ import datetime
 import argparse
 
 def count_triple_threes(start, stop):
+    # Count all even numbers containing three 3's between two numbers.
+    # Provided "start" must be even.
+    # TODO Add test that start is even
     count = 0
     for num in range(start, stop, 2):
         if (test_for_three_threes_mod(num)):
@@ -11,6 +14,7 @@ def count_triple_threes(start, stop):
     return count
 
 def test_for_three_threes_mod(num):
+    # Test if a provided number contains three 3's (they do not need to be adjacent)
     three_count = 0
     while (num > 0):
         if (num % 10) == 3:
@@ -25,16 +29,21 @@ def main():
     argParser.add_argument("-d", "--digits", help="Digit Count")
     args = argParser.parse_args()
     digits = int(args.digits)
+
+    # Start log output
+    print("")
     print(f"Digit Count: {digits}")
     print(f"starting: {datetime.datetime.now()}")
+
+    # Run calculation w/timer
     start = time.time()
     count = count_triple_threes(10**(digits - 1), 10**digits)
     end = time.time()
+
+    # Finish log output
     print(f"Complete: {datetime.datetime.now()}")
-
-    duration = datetime.timedelta(seconds = end - start)
-    print(f"Duration: {duration}")
-
+    print(f"Duration: {datetime.timedelta(seconds = end - start)}")
     print(f"Count: {count}")
+
 
 main()
